7f18992a5f0da27eb280d4e074672f1ef5602cee
Climax708 <soryy708@gmail.com>
2023-05-23 23:29:11 +0300

    optimize

--- src/front/game/ai/solver.ts
+++ src/front/game/ai/solver.ts
@@ -6,17 +6,22 @@ export class AiSolver {
     constructor(private board: Board) {}
 
     solveNextTurn(_piece: GamePiece): { x: BoardPosition; y: BoardPosition } {
-        // eslint-disable-next-line no-constant-condition
-        while (true) {
-            const randomX: BoardPosition = Math.floor(
-                Math.random() * 4,
-            ) as never;
-            const randomY: BoardPosition = Math.floor(
-                Math.random() * 4,
-            ) as never;
-            if (this.board.canStack(randomX, randomY)) {
-                return { x: randomX, y: randomY };
-            }
-        }
+        const allValid = this.getAllValidPlays(this.board);
+        return allValid[Math.floor(Math.random() * allValid.length)];
+    }
+
+    private getAllValidPlays(
+        board: Board,
+    ): Array<{ x: BoardPosition; y: BoardPosition }> {
+        const zeroToThree = [0, 1, 2, 3];
+        return zeroToThree
+            .map((x) =>
+                zeroToThree.map((y) => ({
+                    x: x as BoardPosition,
+                    y: y as BoardPosition,
+                })),
+            )
+            .reduce((prev, cur) => [...prev, ...cur])
+            .filter(({ x, y }) => board.canStack(x, y));
     }
 }
